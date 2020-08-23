bf11e8ddf07b741f865de5c38468d38ef10d1d68
aithasaikumar <aithasaikumar121@gmail.com>
2020-08-23 11:15:23 +0530

    initilization started

---
+++ src/components/Footer/FooterPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './FooterPage.module.css'
+
+const FooterPage = () => {
+  return (
+    <div className={styles.container}>
+        <div>
+          <p className={styles.tag}> Corona Virus Live Tracking</p>
+        </div>
+    </div>
+  );
+}
+
+export default FooterPage;
